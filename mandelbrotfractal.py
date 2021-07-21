c8830c93d08b7bfd90cc57550bb2af5363dcfafb
= <pmch02@protonmail.com>
2021-07-20 22:50:46 -0400

    Tweak mb for smooth coloring

--- mandelbrotfractal.py
+++ mandelbrotfractal.py
@@ -1,14 +1,17 @@
 from math import log, log2
 
 # Return iterations needed to reach mod > 2
-MAX_ITERATIONS = 100
+MAX_ITERATIONS = 80
 def mandelbrot(c):
     z = 0
     n = 0
     while abs(z) <= 2 and n < MAX_ITERATIONS:
         z = z * z + c
         n += 1
-    return n
+    if n == MAX_ITERATIONS:
+        return MAX_ITERATIONS
+
+    return n + 1 - log(log2(abs(z)))
 
 if __name__ == "__main__":
     for a in range(-10, 10, 5):
