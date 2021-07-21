59f315646a96f0aff12edade6b2cb546425bf326
= <pmch02@protonmail.com>
2021-07-21 11:54:51 -0400

    Further semantic changes

--- mandelbrotfractal.py
+++ mandelbrotfractal.py
@@ -22,11 +22,3 @@ class MdFractal:
         if n == self.MAX_ITERATIONS:
             return self.MAX_ITERATIONS
         return n + 1 - log(log2(abs(z)))
-
-'''
-if __name__ == "__main__":
-    for a in range(-10, 10, 5):
-        for b in range(-10, 10, 5):
-            c = complex(a / 10, b / 10)
-            print(c, MdFractal().mandelbrot(c))
-'''
